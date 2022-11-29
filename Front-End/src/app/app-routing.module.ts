95679fca044be7f81280f8defe637a27c0a2f9a2
khayoussef-mohamed <mohamed.khayoussef17@gmail.com>
2022-11-29 17:36:53 +0100

    ajout du ng zorro

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'header', component: HeaderComponent }];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -9,12 +9,25 @@ import { SideMenuItemComponent } from './pages/side-menu/side-menu-item/side-men
 import { MenuComponent } from './pages/menu/menu.component';
 import { MenuItemComponent } from './pages/menu/menu-item/menu-item.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { fr_FR } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import fr from '@angular/common/locales/fr';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+
+registerLocaleData(fr);
 
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, SideMenuComponent, SideMenuItemComponent, MenuComponent, MenuItemComponent],
-  imports: [BrowserModule, AppRoutingModule, MatCardModule, BrowserAnimationsModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, MatCardModule, BrowserAnimationsModule, FormsModule, HttpClientModule, IconsProviderModule, NzLayoutModule, NzMenuModule],
+  providers: [
+    { provide: NZ_I18N, useValue: fr_FR }
+  ],
   bootstrap: [AppComponent],
 
 })
