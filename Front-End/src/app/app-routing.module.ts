b35bfe4b2f1074b15f1b4053f430c48cd88a83f3
FerdaoussCHAKROUN <71392449+FerdaoussCHAKROUN@users.noreply.github.com>
2022-12-11 17:24:30 +0100

    update menu

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
+import {MenuComponent} from "./pages/menu/menu.component";
 
 const routes: Routes = [
   { path: 'header', component: HeaderComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
 ];
 
 @NgModule({
