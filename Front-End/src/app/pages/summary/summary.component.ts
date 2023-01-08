d05027c11ef9e084b5c34b54b8b61f5cce26aa19
KHAYOUSSEF MOHAMED <54961688+khayoussef-mohamed@users.noreply.github.com>
2023-01-08 22:11:39 +0100

    CSS

--- Front-End/src/app/pages/summary/summary.component.ts
+++ Front-End/src/app/pages/summary/summary.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {delay} from "rxjs";
 
 @Component({
   selector: 'app-summary',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./summary.component.css']
 })
 export class SummaryComponent {
+   status = false;
+ngOnInit() {
+  this.changeStatus()
+}
+
+changeStatus(){
+  this.delay(15000).then(() => this.status = true)
+}
+ delay(milliseconds : number) {
+    return new Promise(resolve => setTimeout( resolve, milliseconds));
+  }
 
 }
